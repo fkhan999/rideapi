ccea7f84aebab62a1c1f5a43bfbcc8c0c9118a65
Farrukh khan <farrukh.khan999@gmail.com>
2022-10-16 13:46:04 +0530

    variable name changed and defined the status code

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 #from django.http import JsonResponse
+from urllib import response
 from django.forms.models import model_to_dict
 import json
 from api.models import asset_transport_request,rider_request
@@ -37,16 +38,16 @@ def asset_req(request,*args,**kwargs):
         print(a)
         format="%Y-%m-%d %H:%M:%S"
         if a["asset_type"].upper() not in valid_asset_type:
-            return Response({"message":a['asset_type']+" is not valid assest type"})
+            return Response({"message":a['asset_type']+" is not valid assest type"},status=400)
         if a["sensitivity"].upper() not in valid_sensitivity:
-            return Response({"message":a['sensitivity']+" is not valid sensitivity"})
-        asset_transport_request.objects.create(user_req=a["user"],from_req=a["from"],to_req=a["to"],date_time_req=datetime.datetime.strptime(a['date_time'],format),is_flex_req=a["is_flex"],quantity_req=a["quantity"],asset_type=a["asset_type"],asset_type_sensitivity=a["sensitivity"],deliver=a['deliver'])
+            return Response({"message":a['sensitivity']+" is not valid sensitivity"},status=400)
+        response_from_db=asset_transport_request.objects.create(user_req=a["user"],from_req=a["from"],to_req=a["to"],date_time_req=datetime.datetime.strptime(a['date_time'],format),is_flex_req=a["is_flex"],quantity_req=a["quantity"],asset_type=a["asset_type"],asset_type_sensitivity=a["sensitivity"],deliver=a['deliver'])
     except Exception as e:
         print(e)
-        return Response({"message":"data not valid"})
-    data={"message":"request successfully added"}
-    data['data']=a
-    return Response(data)
+        return Response({"message":"data not valid"},status=400)
+    data={"message":"Request successfully added"}
+    data['data']="Request id is "+str(response_from_db.id)
+    return Response(data,status=200)
 
 
 #This view is for creating new ride information request by rider
@@ -57,14 +58,13 @@ def rider_req(request,*args,**kwargs):
         print(a)
         format="%Y-%m-%d %H:%M:%S"
         if a["medium"].upper() not in valid_travel_mediums:
-            return Response({"message":a['medium']+" is not valid travel medium"},status=500)
-        rider_request.objects.create(user_rider=a["user"],from_rider=a["from"],to_rider=a["to"],date_time_rider=datetime.datetime.strptime(a['date_time'],format),is_flex_rider=a["is_flex"],quantity_rider=a["quantity"],medium_rider=a["medium"])
+            return Response({"message":a['medium']+" is not valid travel medium"},status=400)
+        response_from_db=rider_request.objects.create(user_rider=a["user"],from_rider=a["from"],to_rider=a["to"],date_time_rider=datetime.datetime.strptime(a['date_time'],format),is_flex_rider=a["is_flex"],quantity_rider=a["quantity"],medium_rider=a["medium"])
     except:
-        return Response({"message":"data not valid"},status=500)
-    data={"message":"request successfully added"}
-    data['data']=a
-
-    return Response(data)
+        return Response({"message":"data not valid"},status=400)
+    data={"message":"Request successfully added"}
+    data['data']="Request id is "+str(response_from_db.id)
+    return Response(data,status=200)
 
 #This view is for viewing request sent by a requester
 @api_view(['GET'])
@@ -74,8 +74,7 @@ def asset_req_get(request,*args,**kwargs):
         print(a)
         user=a['user']
     except:
-        return Response({"message":"Invalid parameter"},status=500)
-
+        return Response({"message":"Invalid parameter"},status=400)
     data=asset_transport_request.objects.filter(user_req=user)
     try:
         data=data.filter(asset_type=a['asset_type'].upper())
@@ -83,7 +82,7 @@ def asset_req_get(request,*args,**kwargs):
         pass
 
     if not data:
-        return Response({"message":"Either no request for this user or no request with this filter"},status=500)
+        return Response({"message":"Either no request for this user or no request with this filter"},status=404)
     res={"message":"success","data":[]}
     res1=[]
     for x in data:
@@ -106,10 +105,11 @@ def asset_req_get(request,*args,**kwargs):
         except:
             res1.append(b)
     if len(res1)==0:
-        return Response({"message":"Either no request for this user or no request with this filter"},status=500)
+        return Response({"message":"Either no request for this user or no request with this filter"},status=404)
     res1=sorted(res1, key=lambda i: i['date_time_req'])
     res['data']=res1
-    return Response(res)
+    print(res)
+    return Response(res,status=200)
 
 
 #This view is for  viewing match with rider
@@ -120,47 +120,48 @@ def match(request,*args,**kwargs):
         print(a)
         id_req=a['id']
     except:
-        return Response({"message":"Invalid parameter"},status=500)
+        return Response({"message":"Invalid parameter"},status=400)
     try:
         page=a['page']
+        if page<=0:
+            return Response({"message":"Page value should be greater than zero"},status=400)
     except:
-        page=0
+        page=1
     res={"message":"success","data":[]}
     res1=[]
     try:
         data=asset_transport_request.objects.get(id=id_req)
     except:
-        return Response({"message":"No matching request"},status=500)
+        return Response({"message":"No matching request"},status=404)
 
     data=model_to_dict(data)
-    data=rider_request.objects.filter(from_rider=data['from_req'],to_rider=data['to_req'],date_time_rider=data['date_time_req'])[page*5:page*5+5]
+    data=rider_request.objects.filter(from_rider=data['from_req'],to_rider=data['to_req'],date_time_rider=data['date_time_req'])[(page-1)*5:page*5]
     print(data)
     for x in data:
         b=model_to_dict(x)
         res1.append(b)
     if len(res1)==0:
-        return Response({"message":"No matching request"},status=500)
+        return Response({"message":"No record exists for requested page"},status=404)
     res['data']=res1
-    return Response(res)
+    return Response(res,status=200)
 
 #this view is for selecting a rider out of match
 @api_view(['POST'])
 def rider_select(request,*args,**kwargs):
     try:
-        a=json.loads(request.body)
-        print(a)
-        id_req=a['id']
-        id_select=a['select_id']
+        request_body=json.loads(request.body)
+        request_id=request_body['request_id']
+        ride_id=request_body['ride_id']
     except:
-        return Response({"message":"Invalid parameter"},status=500)
+        return Response({"message":"Invalid parameter"},status=400)
     try:
-        data=asset_transport_request.objects.get(id=id_req)
+        data=asset_transport_request.objects.get(id=request_id)
     except:
-        return Response({"message":"invalid id sent"},status=500)
+        return Response({"message":"No record exists for requester id: "+str(request_id)},status=404)
     try:
-        data1=rider_request.objects.get(id=id_select)
+        rider_request.objects.get(id=ride_id)
     except:
-        return Response({"message":"invalid sender id sent"},status=500)
-    data.status=id_select
+        return Response({"message":"No record exists for rider id: "+str(ride_id)},status=404)
+    data.status=ride_id
     data.save()
-    return Response({"message":"success","data":a})
+    return Response({"message":"Request id "+str(request_id)+" successfully matched with ride id "+str(ride_id)},status=200)
